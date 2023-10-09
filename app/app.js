e89e7c635731279467e135f452771517cbc831f4
CJ-Eilenstine <115022708+CJ-Eilenstine@users.noreply.github.com>
2023-10-09 02:12:46 -0400

    Mobile-Nav

--- app/app.js
+++ app/app.js
@@ -8,3 +8,8 @@ Model.changeRoute();
 $(document).ready(function () {
 initURLListener();
 });
+
+$('.menu-toggle').click(function(){
+    $('.nav').toggleClass("mobile-nav");
+    $(this).toggleClass("is-active");
+});
